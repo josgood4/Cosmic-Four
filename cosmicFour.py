a0683749c6b96854c611bf283c49a11ac3a039f3
josgood4 <jwosgood4@gmail.com>
2018-01-13 16:41:53 -0500

    generated a csv file with outputs up to 1 million so I could generate a graph of the resulting values

--- cosmicFour.py
+++ cosmicFour.py
@@ -1,4 +1,5 @@
 import inflect
+import csv
 
 eng = inflect.engine()
 
@@ -18,9 +19,20 @@ def cosmicFour(num):
   next = countLets(s)
   print(str(num) + " goes to " + str(next))
   return cosmicFour(next)
+
+def cosmicFourOnce(num):
+  return countLets(eng.number_to_words(num, andword=''))
   
 ##print(eng.number_to_words(99))
 ##print(countLets(eng.number_to_words(99)))
 
+"""
 for i in range(1000000):
   cosmicFour(i)
+"""
+
+with open('out.csv', 'w') as csvfile:
+  writer = csv.writer(csvfile)
+  writer.writerow(["Number", "Result"])
+  for i in range(1000000):
+    writer.writerow([i, cosmicFourOnce(i)])
