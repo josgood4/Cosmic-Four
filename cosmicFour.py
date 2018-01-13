8048a53500efef3520287864ce20b7021e072118
josgood4 <jwosgood4@gmail.com>
2018-01-13 16:19:13 -0500

    I tested up to 1 million and it looks like they all lead back to 4!

--- cosmicFour.py
+++ cosmicFour.py
@@ -1,24 +1,26 @@
 import inflect
 
+eng = inflect.engine()
+
 def countLets(s):
   count = 0
   for i in range(len(s)):
-    if(s[i]!='-' and s[i]!=' '):
+    if(s[i]!='-' and s[i]!=' ' and s[i]!=','):
       count += 1
   return count
 
 def cosmicFour(num):
   if num==4:
-    print("and 4 is cosmic")
+    print("...and 4 is cosmic")
     return None
-  s = eng.number_to_words(num)
-  print(s)
+  s = eng.number_to_words(num, andword='')
+  ##print(s)
   next = countLets(s)
   print(str(num) + " goes to " + str(next))
   return cosmicFour(next)
   
-eng = inflect.engine()
 ##print(eng.number_to_words(99))
 ##print(countLets(eng.number_to_words(99)))
 
-cosmicFour(135)
+for i in range(1000000):
+  cosmicFour(i)
